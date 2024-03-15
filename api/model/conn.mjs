d83d56fdb3b3af34b7b489811ffc71b24bd20714
Bhawani Sankar Choudhury <bhawani.choudhury@oditeksolutions.com>
2024-03-15 11:54:06 +0530

    first commit

--- api/model/conn.mjs
+++ api/model/conn.mjs
@@ -3,8 +3,7 @@ import 'dotenv/config'
 
 export const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
   host: process.env.host,
-  dialect: "mysql",
-  port:28534
+  dialect: "mysql"
 });
 
  export const user = sequelize.define('user',{
